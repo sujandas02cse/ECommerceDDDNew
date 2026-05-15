41bc77d2786fc7345e3eb9b8fc3330876b7b1b1a
Administrator <Administrator@SUJANWINDOWS>
2026-05-15 23:32:37 +0600

    ddsfsdfdsfs

---
+++ ECommerce.Domain/Repositories/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer?> GetByIdAsync(int id);
+        Task AddAsync(Customer customer);
+
+    }
+}
---
+++ ECommerce.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<Order?>GetByIdAsync(int id);
+        Task AddAysnc(Order order);
+    }
+}
