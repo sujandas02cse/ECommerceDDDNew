3cad9164e977075e4558a60ba34f0f3345e6e602
Administrator <Administrator@SUJANWINDOWS>
2026-05-16 22:04:46 +0600

    sdfsdfsd

---
+++ ECommerce.Domain/Services/OrderDomainService.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Services
+{
+    public class OrderDomainService
+    {
+        public bool CanPlaceOrder(Customer customer,List<OrderItem> items) 
+        {
+            return customer != null && items != null && items.Count > 0;
+
+        }
+    }
+}
