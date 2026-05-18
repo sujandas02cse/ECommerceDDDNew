a77b0f369b82360575ea00540db95af81474e069
Administrator <Administrator@SUJANWINDOWS>
2026-05-18 07:07:17 +0600

    add order repository

---
+++ ECommerce.Insfrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Repositories;
+using ECommerce.Insfrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Insfrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly ECommerceDbContext _context;
+
+        public OrderRepository(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task AddAysnc(Order order)
+        {
+           await _context.Orders.AddAsync(order);
+           await _context.SaveChangesAsync();
+        }
+
+        public async Task<Order?> GetByIdAsync(int id)
+        {
+            return await _context.Orders.Include(o => o.Items)
+                                        .AsNoTracking()
+                                        .FirstOrDefaultAsync(o => o.Id == id);
+        }
+    }
+}
