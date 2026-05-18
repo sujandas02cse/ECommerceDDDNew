2a0f130e5c6d124787e973ed7efbd3ea247b5557
Administrator <Administrator@SUJANWINDOWS>
2026-05-18 06:58:57 +0600

    add product repository get all method

---
+++ ECommerce.Insfrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Repositories;
+using ECommerce.Insfrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Insfrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ECommerceDbContext _context;
+
+        public ProductRepository(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
