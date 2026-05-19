5fe795da30863d5f9d874d45f49564f5057df5fa
Administrator <Administrator@SUJANWINDOWS>
2026-05-19 22:42:28 +0600

    customer repository added

---
+++ ECommerce.Application/DTOs/CreateProductDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ECommerce.Application.DTOs
+{
+    public class CreateProductDTO
+    {
+        [Required,MaxLength(200)]
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        [Range(0.01,double.MaxValue)]
+        public decimal price { get; set; }
+
+        [Range(0,int.MaxValue)]
+        public  int  StockQuantity { get; set; }
+    }
+}
---
+++ ECommerce.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+
+
+    }
+}
