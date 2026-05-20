43f78fb2c5eb058f1cb4bd743562812e6461fac3
Administrator <Administrator@SUJANWINDOWS>
2026-05-20 08:28:26 +0600

    product service added

---
+++ ECommerce.Application/DTOs/OrderDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application.DTOs
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public AddressDTO ShippingAddress { get; set; } = null!;
+        public List<OrderItemDTO> Items { get; set; } = new();
+
+
+    }
+}
